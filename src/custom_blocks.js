5999db294a9e26bf6dea0b3ebee81ca3015e50c3
pescalette <78445073+pescalette@users.noreply.github.com>
2023-10-01 22:10:20 -0700

    Got code generation working for all implemented blocks

--- src/custom_blocks.js
+++ src/custom_blocks.js
@@ -18,28 +18,57 @@ document.addEventListener('DOMContentLoaded', function () {
                     },
                     {
                         "kind":"block",
-                        "type":"waypoint_control"
+                        "type":"coordinate_input"
                     },
                     {
                         "kind":"block",
-                        "type":"coordinate_input"
+                        "type":"waypoint_control"
                     },
-
+                ]
+            },
+            {
+                "kind":"category",
+                "name":"Logic",
+                "contents":[
+                    {
+                        "kind":"block",
+                        "type":"controls_repeat_custom"
+                    },
                 ]
             }
         ]
     }
 
+
+    Blockly.Blocks['controls_repeat_custom'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField("Repeat")
+                .appendField(new Blockly.FieldNumber(0), "TIMES")
+                .appendField("times");
+            this.appendStatementInput("DO")
+                .appendField("Do:")
+                .setCheck("move"); // Replace with your desired block type
+            this.setInputsInline(true);
+            this.setPreviousStatement(true, ["controls_repeat_custom", "move"]);
+            this.setNextStatement(true, ["controls_repeat_custom", "move"]);
+            this.setColour(20);
+            this.setTooltip("Repeat a set of actions a specified number of times.");
+            this.setHelpUrl("");
+        }
+    };
+
+
     Blockly.Blocks['move'] = {
         init: function() {
             this.appendDummyInput()
-                .appendField("Move");
+                .appendField("Move to");
             this.appendStatementInput('WAYPOINT')
                 .setCheck('waypoint')
                 .appendField('Waypoint:');
-            this.setPreviousStatement(true, null);
-            this.setNextStatement(true, null);
-            this.setColour(160); // Change the color to a different value
+            this.setPreviousStatement(true, "move");
+            this.setNextStatement(true, "move");
+            this.setColour(40);
             this.setTooltip('Moves the robot through the inserted waypoints');
         }
     };
@@ -52,36 +81,16 @@ document.addEventListener('DOMContentLoaded', function () {
                 .setCheck('waypoint_destination')
                 .appendField('Coordinates to move to:');
             this.appendValueInput('WAYPOINT_CONTROLS')
-                .setCheck(['waypoint_controls'])
+                .setCheck('waypoint_controls')
                 .appendField('Optional Controls:');
             this.setPreviousStatement(true, 'waypoint');
             this.setNextStatement(true, 'waypoint');
-            this.setColour(210); // Change the color to a different value
+            this.setColour(60);
             this.setTooltip('Create a waypoint with X, Y, and Z coordinates and optional control blocks.');
         }
     };
-    
-    Blockly.Blocks['waypoint_control'] = {
-        init: function() {
-            this.appendDummyInput()
-                .appendField('Linear Acceleration:')
-                .appendField(new Blockly.FieldNumber(0), 'ACCELERATION')
-                .setAlign(Blockly.ALIGN_RIGHT);
-            this.appendDummyInput()
-                .appendField('Joint Acceleration:')
-                .appendField(new Blockly.FieldNumber(0), 'ACCELERATION')
-                .setAlign(Blockly.ALIGN_RIGHT);
-            this.appendDummyInput()
-                .appendField('Speed:')
-                .appendField(new Blockly.FieldNumber(0), 'SPEED')
-                .setAlign(Blockly.ALIGN_RIGHT);
-            this.setOutput(true, 'waypoint_controls');
-            this.setColour(260); // Change the color to a different value
-            this.setTooltip('Set acceleration and speed for the waypoint.');
-        }
-    };
-    
-    Blockly.Blocks['coordinate_input'] = {
+
+        Blockly.Blocks['coordinate_input'] = {
         init: function() {
             this.appendDummyInput()
                 .appendField("Coords:")
@@ -92,17 +101,94 @@ document.addEventListener('DOMContentLoaded', function () {
                 .appendField("Z:")
                 .appendField(new Blockly.FieldNumber(0), 'Z');
             this.setOutput(true, 'waypoint_destination');
-            this.setColour(120); // Change the color to a different value
+            this.setColour(80);
             this.setTooltip('Specify X, Y, and Z coordinates.');
         }
     };
     
-      
+    Blockly.Blocks['waypoint_control'] = {
+        init: function() {
+            this.appendDummyInput()
+                .appendField('Acceleration:')
+                .appendField(new Blockly.FieldNumber(0), 'ACCELERATION')
+                .setAlign(Blockly.ALIGN_RIGHT);
+            this.appendDummyInput()
+                .appendField('Speed:')
+                .appendField(new Blockly.FieldNumber(0), 'SPEED')
+                .setAlign(Blockly.ALIGN_RIGHT);
+            this.setOutput(true, 'waypoint_controls');
+            this.setColour(80); 
+            this.setTooltip('Set acceleration and speed for the waypoint.');
+        }
+    };
+    
+
 
 
     const workspace = Blockly.inject('blocklyDiv', {
         media: '../lib/blockly/media/',
         toolbox: toolbox,
+        toolboxPosition: 'start',
     });
 
+
+
+    function generatePythonCode() {
+
+
+        const code = python.pythonGenerator.workspaceToCode(workspace);
+    
+        // Add import statements and robot initialization
+        let pythonCode = 'import webots\n\n';
+        pythonCode += 'robot = webots.Robot()\n';
+        pythonCode += 'motor = robot.getMotor("motor_name")\n\n';
+    
+        // Concatenate the generated Python code
+        pythonCode += code;
+        document.getElementById('generated-code').innerText = pythonCode;
+        console.log(pythonCode);
+    }
+    
+    // Define the Python code generation for each block type
+    Blockly.Python.forBlock['move'] = function (block) {
+        const waypointsCode = Blockly.Python.statementToCode(block, 'WAYPOINT');
+        return waypointsCode;
+    };
+    
+    Blockly.Python.forBlock['waypoint'] = function (block) {
+        const coordinatesCode = Blockly.Python.valueToCode(block, 'COORDINATES', Blockly.Python.ORDER_ATOMIC);
+        const waypointControlsCode = Blockly.Python.valueToCode(block, 'WAYPOINT_CONTROLS', Blockly.Python.ORDER_ATOMIC);
+        
+        const code = 'motor.setPosition(' + coordinatesCode + ')\n';
+        
+        return code;
+    };
+    
+    Blockly.Python.forBlock['coordinate_input'] = function (block) {
+        const x = block.getFieldValue('X');
+        const y = block.getFieldValue('Y');
+        const z = block.getFieldValue('Z');
+        
+        return ['webots.Vector3(' + x + ', ' + y + ', ' + z + ')', Blockly.Python.ORDER_ATOMIC];
+    };
+    
+    Blockly.Python.forBlock['waypoint_control'] = function (block) {
+        const acceleration = block.getFieldValue('ACCELERATION');
+        const speed = block.getFieldValue('SPEED');
+        
+        return [`# Set acceleration and speed: Acceleration: ${acceleration}, Speed: ${speed}`,  Blockly.Python.ORDER_ATOMIC];
+    };
+    
+    Blockly.Python.forBlock['controls_repeat_custom'] = function (block) {
+        const times = block.getFieldValue('TIMES');
+        const doCode = Blockly.Python.statementToCode(block, 'DO');
+        
+        return `for i in range(${times}):\n${doCode}\n`;
+    };
+    
+    // Attach the code generation function to the button click event
+    document.getElementById('generateCodeButton').addEventListener('click', generatePythonCode);
+
+    // keep submenu open
+    workspace.toolbox_.flyout_.autoClose = false;
 });
