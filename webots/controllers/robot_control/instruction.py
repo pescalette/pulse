b097e04cfb5b4f7b4367055f1b0ea88c601d3fdf
pescalette <78445073+pescalette@users.noreply.github.com>
2023-12-05 20:29:26 -0800

    removed debug statements from last commit

--- webots/controllers/robot_control/instruction.py
+++ webots/controllers/robot_control/instruction.py
@@ -45,14 +45,12 @@ class Move(Instruction):
         current_joint_positions = controller.get_current_position()
         # If the robot hasn't yet moved towards the current waypoint, calculate and set target positions
         if not hasattr(self, 'target_joint_positions'):
-            print('target set')
             self.robot_startup_delay = 5
             target_position = np.array(waypoint.coords)
             self.target_joint_positions = controller.chain.inverse_kinematics(target_position)[:len(controller.motors) + 1]
             # When the IK solution is not feasible (the position is out of range), it might return an empty list or None,
             # handle this case by treating the waypoint as reached and moving on to the next waypoint
             if self.target_joint_positions is None or np.isnan(self.target_joint_positions).any():
-                print('target out of range')
                 self.current_waypoint_index += 1
                 return False
             
@@ -65,7 +63,6 @@ class Move(Instruction):
             else:
                 # Check if the robot is still moving towards the previous target
                 if not self.is_robot_moving(self.previous_joint_positions, current_joint_positions):
-                    print('robot not moving')
                     # If the robot is not moving, we consider the waypoint unreachable and move on
                     self.current_waypoint_index += 1
                     del self.target_joint_positions
